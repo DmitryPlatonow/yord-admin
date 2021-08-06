4691270c9ead9fbb87462f3b948b36bad11e4392
Dzmitry_Platonau <dzmitry_platonau@epam.com>
2021-08-06 20:15:34 +0300

    Updates

---
+++ precache-manifest.43cb7ea0782f96bbb459882f74d95651.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d9d295858de07259bb960a2441f52a",
+    "url": "/DmitryPlatonow/yord-admin/index.html"
+  },
+  {
+    "revision": "c943e87fca60f1a949c4",
+    "url": "/DmitryPlatonow/yord-admin/static/js/2.7df6fff6.chunk.js"
+  },
+  {
+    "revision": "3209d205d74adb0ac79269340686cd41",
+    "url": "/DmitryPlatonow/yord-admin/static/js/2.7df6fff6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dfc4df24ccfc7ad7512",
+    "url": "/DmitryPlatonow/yord-admin/static/js/main.0cde9a09.chunk.js"
+  },
+  {
+    "revision": "b6557a9b980a690ace4e",
+    "url": "/DmitryPlatonow/yord-admin/static/js/runtime-main.c8b474ec.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/DmitryPlatonow/yord-admin/precache-manifest.43cb7ea0782f96bbb459882f74d95651.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DmitryPlatonow/yord-admin/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
