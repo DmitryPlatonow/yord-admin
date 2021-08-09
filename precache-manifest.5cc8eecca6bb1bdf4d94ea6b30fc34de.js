ce7b14089a232eb3acd044e70cc93a6d94949eca
Dzmitry_Platonau <dzmitry_platonau@epam.com>
2021-08-09 08:50:56 +0300

    Updates

---
+++ precache-manifest.5cc8eecca6bb1bdf4d94ea6b30fc34de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fce25f27fc8f76425b58c90ebecf8bdb",
+    "url": "/DmitryPlatonow/yord-admin/index.html"
+  },
+  {
+    "revision": "226451b80f7c2654b803",
+    "url": "/DmitryPlatonow/yord-admin/static/js/2.8e2f6d08.chunk.js"
+  },
+  {
+    "revision": "3209d205d74adb0ac79269340686cd41",
+    "url": "/DmitryPlatonow/yord-admin/static/js/2.8e2f6d08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63673f67308bacc36581",
+    "url": "/DmitryPlatonow/yord-admin/static/js/main.cbbe6e51.chunk.js"
+  },
+  {
+    "revision": "b6557a9b980a690ace4e",
+    "url": "/DmitryPlatonow/yord-admin/static/js/runtime-main.c8b474ec.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/yord-admin/precache-manifest.29ad59b3c93110a10c444970ded4cb79.js"
+  "/DmitryPlatonow/yord-admin/precache-manifest.5cc8eecca6bb1bdf4d94ea6b30fc34de.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yord-admin/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DmitryPlatonow/yord-admin/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
