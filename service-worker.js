da80c278f62dc3ae34732c7addf7be180019d7ee
Dzmitry_Platonau <dzmitry_platonau@epam.com>
2021-08-10 21:11:35 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/DmitryPlatonow/yord-admin/precache-manifest.5cc8eecca6bb1bdf4d94ea6b30fc34de.js"
+  "/yord-admin/precache-manifest.36fcc514322a749fff4874270066dd5d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DmitryPlatonow/yord-admin/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yord-admin/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
